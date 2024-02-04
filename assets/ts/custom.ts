49c085230cec09ac0cd92f0f7f75097fbb3bc5ff
MegaOwIer <megaowier@gmail.com>
2024-02-05 03:22:58 +0800

    feat: Appearance improvment.  * Change color of highlighted line of code snippet in dark mode.  * Change background to randomly selected picture.  * Add opacaity to main body of site.  * Reduce width of right sidebar.

--- assets/ts/custom.ts
+++ assets/ts/custom.ts
@@ -1,1 +1,33 @@
 /* Place your custom TS in HUGO_SITE_FOLDER/assets/ts/custom.ts */
+
+import "ts/jquery/index.d";
+
+var isHorizonal = null;
+
+function choose_background() {
+    if (isHorizonal == innerWidth >= innerHeight) {
+        return;
+    }
+    isHorizonal = innerWidth >= innerHeight;
+
+    var images_horizonal = ['pixiv-49383596', 'pixiv-57793944'];
+    var images_vertical = ['pixiv-113819513', 'pixiv-114090839', 'pixiv-114204163'];
+
+    var images = isHorizonal ? images_horizonal : images_vertical;
+
+    var randomNumber = Math.floor(Math.random() * images.length);
+    var bgImg = 'url(/background/' + images[randomNumber] + '.jpg)';
+
+    $('body').css({
+        'background-image': bgImg,
+        'background-attachment': 'fixed',
+        'background-blend-mode': 'overlay',
+        'background-size': 'cover',
+        'background-position': 'center'
+    });
+
+    console.log("Background file name: " + images[randomNumber]);
+}
+
+$(choose_background);
+window.addEventListener("resize", choose_background);
